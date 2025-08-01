70e6227ddb027584f129d888b29e9afbbd9b380c
sachin <sachinswatz1798@gmail.com>
2025-08-01 18:36:52 +0530

    I had created the folder of python and saved two programs

---
+++ Averages.py
@@ -0,0 +1,7 @@
+print("This is the program for averages")
+x = int(input("Enter Three Numbers to find Averages: "))
+y = int(input())
+z = int(input())
+avg = x+y+z/3
+print("The average of the given numbers is :",str(avg))
+
