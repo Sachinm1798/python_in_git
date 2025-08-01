fcc281036873aed40aeac250be9a7a6840bedb85
sachin <sachinswatz1798@gmail.com>
2025-08-01 19:12:31 +0530

    deleted one file

--- negt_post.py
+++
@@ -1,6 +0,0 @@
-print("Program to find out whether the given number is Negative or positive")
-x = int(input("Enter the Number : "))
-if x < 0 :
-    print("The given number is Negative Number")
-else:
-    print("The given number is Positive number")
