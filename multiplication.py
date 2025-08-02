dbd5bb0306bda3dc876d9ab7524408e4b4de1295
sachin <sachinswatz1798@gmail.com>
2025-08-02 12:06:27 +0530

    Multiplication program

---
+++ multiplication.py
@@ -0,0 +1,10 @@
+print("This is a program to print the multiplication tables\n")
+n = int(input("Enter the number to find the multiplication table : "))
+l = int(input("\nEnter the limit :"))
+i = 1
+while i <= l:
+    m = i*n
+    print(m)
+    i = i+1
+print("Print Completed")
+
